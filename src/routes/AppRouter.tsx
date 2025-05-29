72ba965caa01ca2b9a81ef0579a83dd55ecdec4a
GusBauAr <gustavo.bautista.arce.2000@gmail.com>
2025-05-29 14:22:16 -0400

    implement NavBar Material UI

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,20 +1,17 @@
-import { Route, Routes } from "react-router-dom"
-import { HomePage } from "../pages/HomePage"
-import { GuidePage } from "../pages/GuidePage"
-import { ExperiencesPage } from "../pages/ExperiencesPage"
-import { RecycleMapPage } from "../pages/RecycleMapPage"
+import { Routes, Route } from 'react-router-dom';
+import { HomePage } from '../pages/HomePage';
+import { GuidePage } from '../pages/GuidePage';
+import { ExperiencesPage } from '../pages/ExperiencesPage';
+import { RecycleMapPage } from '../pages/RecycleMapPage';
 
 
-
-export const AppRouter = () => {
+export default function AppRouter() {
   return (
     <Routes>
-        <Route path="/" element={<HomePage/>} />
-        <Route path="/guide" element={<GuidePage/>} />
-        <Route path="/experiences" element={<ExperiencesPage/>} />
-        <Route path="/recycleMap" element={<RecycleMapPage/>} />
-
-
+      <Route path="/" element={<HomePage />} />
+      <Route path="/guide" element={<GuidePage />} />
+      <Route path="/experiences" element={<ExperiencesPage />} />
+      <Route path="/recycle-map" element={<RecycleMapPage />} />
     </Routes>
-  )
+  );
 }
