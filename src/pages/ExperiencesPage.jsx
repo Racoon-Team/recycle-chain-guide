3790dfb6f7068be2b0e45ae332d73f90df02cb7c
GusBauAr <gustavo.bautista.arce.2000@gmail.com>
2025-05-29 09:33:17 -0400

    File structure

---
+++ src/pages/ExperiencesPage.jsx
@@ -0,0 +1,6 @@
+
+export const ExperiencesPage = () => {
+  return (
+    <div>ExperiencesPage</div>
+  )
+}
---
+++ src/pages/RecycleMapPage.jsx
@@ -0,0 +1,6 @@
+
+export const RecycleMapPage = () => {
+  return (
+    <div>RecycleMapPage</div>
+  )
+}
