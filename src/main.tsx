3741a57f4aef8d29cb88a04b162704758f63e797
Josh <901211+Jhegs@users.noreply.github.com>
2025-06-02 11:59:41 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-// import RecycleApp from "./RecycleApp.tsx";
-import { LoginPage } from './auth/pages/LoginPage';
+import RecycleApp from "./RecycleApp.tsx";
+// import { LoginPage } from './auth/pages/LoginPage';
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <StrictMode>
-      {/* <RecycleApp /> */}
-      <LoginPage />
+      <RecycleApp />
+{/*       <LoginPage /> */}
     </StrictMode>
   </BrowserRouter>
 );
