734c8f05a1b7688ac85521da7ddafc69ad043883
GusBauAr <gustavo.bautista.arce.2000@gmail.com>
2025-06-03 10:26:18 -0400

    feat #16 creating singInWithGoogle and creating file providers, store and thunks

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,19 @@
+
 import { StrictMode } from 'react';
-
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import RecycleApp from './RecycleApp.tsx';
-
 import './i18n/config.ts';
-
-// import { LoginPage } from './auth/pages/LoginPage';
+import RecycleApp from './RecycleApp.tsx';
+import { store } from './store/store';
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <StrictMode>
-      <RecycleApp />
+  <Provider store={store}>
+    <BrowserRouter>
+      <StrictMode>
+        <RecycleApp />
+      </StrictMode>
+    </BrowserRouter>
+  </Provider>
 
-      {/*       <LoginPage /> */}
-    </StrictMode>
-  </BrowserRouter>
 );
