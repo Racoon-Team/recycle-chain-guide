622bc5065c538abf01c4269418c1fcb3d305b255
Jhegs <jhegsbond@gmail.com>
2025-06-03 17:20:01 -0400

    feat: #22, moving firebase config to env file.

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
-
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import './i18n/config.ts';
+
 import RecycleApp from './RecycleApp.tsx';
 import { store } from './store/store';
+
+import './i18n/config.ts';
+
+// import { LoginPage } from './auth/pages/LoginPage';
 
 createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
@@ -15,5 +17,4 @@ createRoot(document.getElementById('root')!).render(
       </StrictMode>
     </BrowserRouter>
   </Provider>
-
 );
