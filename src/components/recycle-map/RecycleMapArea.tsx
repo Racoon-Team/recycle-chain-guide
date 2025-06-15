8a97c34bcb90068461327e097cef2188935ca966
GusBauAr <gustavo.bautista.arce.2000@gmail.com>
2025-06-15 19:51:53 -0400

    feat #44: Implementation of a location icon on the cursor

--- src/components/recycle-map/RecycleMapArea.tsx
+++ src/components/recycle-map/RecycleMapArea.tsx
@@ -193,6 +193,19 @@ export const RecycleMapArea = () => {
               </Popup>
             </Marker>
           ))}
+          {newPointPos && (
+            <Marker
+              position={newPointPos}
+              icon={L.icon({
+                iconUrl:
+                  'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png',
+                iconSize: [25, 41],
+                iconAnchor: [12, 41],
+                popupAnchor: [1, -34],
+                shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
+              })}
+            />
+          )}
           <MapEvents />
         </MapContainer>
       </div>
