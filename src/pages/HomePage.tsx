a8e27807d8950cbe11e16a4e48afc47698130e73
Jhegs <jhegsbond@gmail.com>
2025-06-18 01:52:18 -0400

    feat #15: deleting more files and fix more styles on the menu.

--- src/pages/HomePage.tsx
+++
@@ -1,7 +0,0 @@
-import { useTranslation } from 'react-i18next';
-
-export const HomePage = () => {
-  const { t } = useTranslation();
-
-  return <div className="home-page">{t('main.title')}</div>;
-};
